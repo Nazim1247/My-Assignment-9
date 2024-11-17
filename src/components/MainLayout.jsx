5a9636c4c56c5b189e7db3dedea6677b126ba197
nazim uddin <mdnajim1247@gmail.com>
2024-11-17 21:03:48 +0600

    first commit

---
+++ src/components/MainLayout.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+
+
+const MainLayout = () => {
+    return (
+        <div>
+            MainLayout
+            <Outlet></Outlet>
+
+        </div>
+    );
+};
+
+export default MainLayout;
---
+++ src/routes/Route.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../components/MainLayout";
+import Home from "../components/Home";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <MainLayout></MainLayout>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            }
+        ]
+    }
+])
+
+export default router;
